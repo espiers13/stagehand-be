4c9508c3969cd7ade111c7b6ef1d64c907f8566d
Emily <e.spiers13@gmail.com>
2026-07-15 09:07:46 +0100

    updated data

--- db/seeds/data/development-data/rehearsals.js
+++ db/seeds/data/development-data/rehearsals.js
@@ -3,17 +3,20 @@ const rehearsalsData = [
     production_id: 1,
     title: "Read-through",
     location: "theSpace on the Mile",
-    rehearsal_date: "2026-08-24",
+    date: "2026-08-24",
     start_time: "10:00",
     end_time: "13:00",
+    notes: "First read-through, full company called",
+    called: [1, 2, 3],
   },
   {
     production_id: 1,
     title: "Full run",
     location: "theSpace on the Mile",
-    rehearsal_date: "2026-08-27",
+    date: "2026-08-27",
     start_time: "14:00",
     end_time: "17:00",
+    called: [1, 2, 3],
   },
 ];
 
